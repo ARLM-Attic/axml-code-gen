7ed6791174996375850b9b626d5505a8a67752eb
SND\cansik_cp <SND\cansik_cp@2327b42d-5241-43d6-9e2a-de5ac946f064>
2011-07-04 09:17:05 +0000

    - implemented simple convert logic - tested some AXML files
    git-svn-id: https://axmlcodegen.svn.codeplex.com/svn@76423 2327b42d-5241-43d6-9e2a-de5ac946f064

---
+++ trunk/AXMLCodeGenerator/AXMLCodeGenerator/ControlParser.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using AXMLCodeGenerator.Properties;
+using System.Xml;
+
+namespace AXMLCodeGenerator
+{
+    public class ControlParser
+    {
+        /// <summary>
+        /// Generates a new partial C# class. Every control from the axml
+        /// will be accessable over this class.
+        /// </summary>
+        /// <param name="ViewName">Name of the View</param>
+        /// <param name="ActivityName">Activity name (for partial usage)</param>
+        /// <param name="NameSpace">Namespace of the class</param>
+        /// <param name="aXMLContent">Content of the axml File</param>
+        /// <returns>Designer Class for Mono for Android</returns>
+        public string GenerateClass(string ViewName, string ActivityName, string NameSpace, string aXMLContent)
+        {
+            //Will be used to replace all the patterns
+            StringBuilder designerClass = new StringBuilder(Resources.template_cs);
+
+            //Generate Keys and Values
+            Dictionary<string, string> varList = CreateControlList(aXMLContent);
+
+            //Replacement
+            designerClass.Replace("%NameSpace%", NameSpace);
+            designerClass.Replace("%ActivityName%", ActivityName);
+            designerClass.Replace("%ViewName%", ViewName);
+            designerClass.Replace("%controlVars%", CreateVariableDefinitionList(varList));
+            designerClass.Replace("%VarsInitial%", CreateVariableInitList(varList));
+
+            return designerClass.ToString();
+        }
+
+        /// <summary>
+        /// Creates a directionary of controls
+        /// </summary>
+        /// <param name="axml">Content of the axml File</param>
+        /// <returns>Dictionary with controls and types</returns>
+        private Dictionary<string, string> CreateControlList(string axml)
+        {
+            Dictionary<string, string> controlList = new Dictionary<string, string>();
+            XmlDocument xdoc = new XmlDocument();
+
+            xdoc.LoadXml(axml);
+
+            foreach (XmlElement xE in xdoc.ChildNodes[1].ChildNodes)
+            {
+                if (xE.HasAttribute("android:id"))
+                {
+                    controlList.Add(xE.Attributes["android:id"].Value.Replace("@+id/", ""), xE.Name);
+                }
+            }
+
+            return controlList;
+        }
+
+        private string CreateVariableDefinitionList(Dictionary<string, string> controlDir)
+        {
+            StringBuilder defList = new StringBuilder();
+
+            foreach (KeyValuePair<string, string> varDef in controlDir)
+            {
+                defList.AppendLine("private " + varDef.Value + " " + varDef.Key + ";");
+            }
+
+            return defList.ToString();
+        }
+
+        private string CreateVariableInitList(Dictionary<string, string> controlDir)
+        {
+            StringBuilder initList = new StringBuilder();
+
+            foreach (KeyValuePair<string, string> varDef in controlDir)
+            {
+                initList.AppendLine(varDef.Key + " = FindViewById<" + varDef.Value + ">(Resource.Id." + varDef.Key + ");");
+            }
+
+            return initList.ToString();
+        }
+    }
+}
--- trunk/AXMLCodeGenerator/AXMLCodeGenerator_Test/Program.cs
+++ trunk/AXMLCodeGenerator/AXMLCodeGenerator_Test/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.IO;
 
 namespace AXMLCodeGenerator_Test
 {
@@ -9,6 +10,10 @@ namespace AXMLCodeGenerator_Test
     {
         static void Main(string[] args)
         {
+            string axmlContent = File.ReadAllText("Login.axml");
+            AXMLCodeGenerator.ControlParser cParser = new AXMLCodeGenerator.ControlParser();
+
+            string designerclass = cParser.GenerateClass("Login", "Activity1", "Test", axmlContent);
         }
     }
 }
